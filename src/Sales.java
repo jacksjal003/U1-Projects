f6b430da49d32d5c0172aea09065296e1a911b8e
jacksjal003 <88158678+jacksjal003@users.noreply.github.com>
2021-09-07 12:43:00 -0400

    Updated Sales.java

--- src/Sales.java
+++ src/Sales.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Sales {
 
     /*Write a program, using dialog boxes, which prompts the user to enter a quantity, price, discount(as an integer) and sales tax(as an integer).
@@ -19,10 +21,31 @@ public class Sales {
 
     public static void main(String[] args) {
 
-        double salePrice = 19.99;
-        double discount = 20;
-        int itemsBuying = 5;
-        double salesTax = 7;
+        String salePrice = JOptionPane.showInputDialog("What is your sales price?");
+        String itemsBuying = JOptionPane.showInputDialog("How many items are you going to buy");
+        String discount = JOptionPane.showInputDialog("What is the discount");
+        String salesTax = JOptionPane.showInputDialog("what is your sales tax?");
+
+
+        String message = "hi";
+
+        double salePrice2 = salePrice * itemsBuying;
+        double discountPrice = salePrice2  * discount;
+        String purchasePrice =  discountPrice * 1 + salesTax;
+
+        double firstPrice = salePrice2 - (salePrice2 * discount);
+        double secondPrice = salePrice2 - firstPrice;
+        double finalPrice = secondPrice * salesTax;
+
+        System.out.println(message + " Your sale price is " + salePrice2);
+        System.out.println(message + " Your discount is " + discount);
+        System.out.println(message + " Your tax is "  + salesTax);
+        System.out.println(message + " Your total price is " + finalPrice);
+
+
+
+
+
 
     }
 }
